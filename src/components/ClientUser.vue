82ec70ff9c140a9bb3b82f9adb2f93c5fb6ef77f
高伟 <gaow_1985@163.com>
2019-12-15 22:26:04 +0800

    用户信息
    用户信息

--- src/components/ClientUser.vue
+++ src/components/ClientUser.vue
@@ -1,35 +1,19 @@
 <template>
   <div>
     <van-nav-bar border title="我的"></van-nav-bar>
-    <br>
-    <van-row>
-      <van-col span="4" offset="1">
-        <van-image fit="fill" width="60" height="60" radius="5px" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-      </van-col>
-      <van-col span="18">
-       <van-row>
-         高伟
-       </van-row>
-       <van-row>
-         账号：gaow_1985
-       </van-row>
-      </van-col>
-    </van-row>
+    <UserInfo></UserInfo>
   </div>
 </template>
 
 <script>
 import { NavBar } from 'vant';
-import { Row, Col } from 'vant';
-import { Image } from 'vant';
+import UserInfo from './client_user/UserInfo.vue';
 
 export default {
   name: 'clientUser',
   components: {
     [NavBar.name]: NavBar,
-    [Row.name]: Row,
-    [Col.name]: Col,
-    [Image.name]: Image
+    [UserInfo.name]: UserInfo,
   },
   data () {
     return {
@@ -40,4 +24,5 @@ export default {
 </script>
 
 <style>
+  
 </style>
---
+++ src/components/client_user/UserInfo.vue
@@ -0,0 +1,57 @@
+<template>
+  <van-cell :center="true" is-link>
+    <div>
+      <div class="div-user-img">
+        <van-image fit="fill" width="60px" height="60px" radius="5px" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+      </div>
+      <div class="div-user-info">
+        <div class="div-user-name">高伟</div>
+        <div class="div-user-account">账号：gaow_1985</div>
+      </div>
+      <div class="div-clear"></div>
+    </div>
+  </van-cell>
+</template>
+
+<script>
+import { Cell } from 'vant';
+import { Image } from 'vant';
+
+export default {
+  name: 'UserInfo',
+  components: {
+    [Cell.name]: Cell,
+    [Image.name]: Image    
+  }
+}
+</script>
+
+<style>
+.div-user-img{
+  float: left;
+  width:60px;
+  height:60px;
+}
+
+.div-user-info{
+  float: left; 
+  margin-left: 10px;
+}
+
+.div-clear{
+  clear:both;
+}
+
+.div-user-name{
+  height: 30px;
+  line-height: 30px;
+  font-weight: bolder;
+  font-size: larger;
+}
+
+.div-user-account{
+  height: 30px;
+  line-height: 30px;
+  font-size:small;
+}
+</style>
